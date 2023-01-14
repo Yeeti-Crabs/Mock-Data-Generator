47266bec1e878481b05e65b9d833439af6e572f6
Mihran Baytaryan <mihranbaytaryan@gmail.com>
2023-01-13 17:01:42 -0800

    boilerplate setup

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/api', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use((req, res) => res.status(404).send('This is not the page you\'re looking for...'));
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
