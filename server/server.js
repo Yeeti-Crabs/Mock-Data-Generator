139e548381847ac00da8f2cdec622926c29a1bc9
Mihran Baytaryan <mihranbaytaryan@gmail.com>
2023-01-13 20:03:36 -0800

    pre num dump

--- server/server.js
+++ server/server.js
@@ -1,19 +1,41 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+const apiRouter = require('./apiRouter.js')
 const PORT = process.env.PORT || 3000
 
-app.get('/api', (req, res) => {
-  res.send('Hello World!')
-})
+const MONGO_URI = 'mongodb+srv://user:user@our-mockdata-storage.ghivuab.mongodb.net/?retryWrites=true&w=majority';
 
+// account for incoming data
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// connection to mongo db
+mongoose.set("strictQuery", false)
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, 
+})
+.then(() => console.log('db connection open'))
+.catch((err) => console.log('error db connection: ', err))
+
+//route to apiRouter
+app.use ('/api', apiRouter)
+
+
+// handle all unknown routes
 app.use((req, res) => res.status(404).send('This is not the page you\'re looking for...'));
 
+//global error handler 
 app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
     status: 500,
     message: { err: 'An error occurred' },
   };
+  if (err.type === 'redirect') {
+    res.redirect(err.url)
+  }
   const errorObj = Object.assign({}, defaultErr, err);
   console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
