4504ddc1db755e95233d4756bce05885bfebaf4b
trevor ferguson <trevorjadeferguson@gmail.com>
2023-01-14 16:43:50 -0800

    updating my branch

---
+++ client/containers/MainContainer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const MainContainer = () => {
+  
+  return(
+    <div id="main">
+      <div id="quantity_container"></div>
+
+    </div>
+  )
+}
+
+
+
+export default MainContainer;
