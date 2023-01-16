0080e2d7d47fb798cfcc1fcc9bbfdfd25414984d
trevor ferguson <trevorjadeferguson@gmail.com>
2023-01-15 23:20:20 -0800

    recent progress on frontend

--- client/components/dataSelector.jsx
+++ client/components/dataSelector.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-
-
-
-
-
-
-export default DataSelector;
+export default function DataSelector({ dataTypes, onDelete, key, id}) {
+  
+  return (
+    <div className='data-type'>
+        <select>
+          <option id='full-name'>Full Name</option>
+          <option id='email'>Email</option>
+        </select>
+        <button onClick = {onDelete}>X</button>
+    </div>
+  )
+}
--- client/containers/mainContainer.jsx
+++ client/containers/mainContainer.jsx
@@ -3,26 +3,50 @@
  * @description Stateful container for functional compononents
  */
 
-import React from 'react';
-// import DatatypeSelector from '../components/dataSelector'
+import React, { useEffect, useState, useRef } from 'react';
+import DataSelector from '../components/DataSelector.jsx'
 
 const MainContainer = () => {
+
+  const [dataTypes, setDataTypes] = useState([])
+  const dataInput = useRef([])
+
+  const submitHandler = (event) => {
+    const allDataTypes = [];
+    dataTypeList.forEach((dataSelector) => {
+    })  
   
+
+  // made a hook for an array of DataType components 
+  const [dataTypeList, setDataTypeList] = useState([<DataSelector dataTypes={dataTypes} 
+                                                      key = {0} 
+                                                      delete = {deleteDataHandler}
+                                                      className = {'data-selector'}/>])
+  const addDataHandler = (event) =>{
+    if (dataTypeList.length < 6){
+      setDataTypeList(dataTypeList.concat(<DataSelector dataTypes={dataTypes} key = {dataTypeList.length} onDelete = {deleteDataHandler} className = {'data-selector'}/>))
+    } 
+  }
+  const deleteDataHandler = (event) => {
+    // if (dataTypeList.length < 1){
+    //   setDataTypeList(dataTypeList.filter((element) => {})
+    // }
+    console.log(event)
+  }
+
+ 
+  }
 
   return (
     <div id="main_container">
-      <div id="quantity_selector">
-        <input type="number" />
-      </div>
+      <input id="quantity_selector" type="number" />
       <div id="datatype_selector">
-        {/* <DatatypeSelector /> */}
+        {dataTypeList}
       </div>
-      <div id="submit_button">
-        <button>Get Data</button>
-      </div>
-      <div id="text_output">
-        <textarea></textarea>
-      </div>
+      {/* make a button to add new DataType */}
+      <button onClick={addDataHandler}>Add Data Type</button>
+        <button onClick = {submitHandler} id="submit_button">Generate Data</button>
+        <textarea id="text_output"></textarea>
     </div>
   )
 };
