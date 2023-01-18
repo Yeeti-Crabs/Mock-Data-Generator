c39464efbe1f8c3477a41871ceae5ec4c8fa8cc6
utyvert <utkarshuppal@gmail.com>
2023-01-18 09:12:54 -0800

    ui work

--- client/containers/headerContainer.jsx
+++ client/containers/headerContainer.jsx
@@ -1,9 +1,14 @@
 import React, {useState} from 'react';
 
+
 const HeaderContainer = () => { 
     return (
       <header id='header'>
         <h1>Mock Data Generator</h1>
+        <div className="header-end">
+          <button>Login</button>
+          <button>Sign Up</button>
+        </div>
       </header>
     )
 }
