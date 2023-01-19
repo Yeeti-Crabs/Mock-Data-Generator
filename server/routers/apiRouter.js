9df185a766ee54ae9688f9f4c8532381fe8fcf36
taylrzhang <neil0725@hotmail.com>
2023-01-18 20:34:16 -0800

    get random data from fakerapi complete

--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const dbController = require("../controllers/dbController");
+const apiController = require("../controllers/apiController");
 
 // dbController is an array of all of our controller functions, NOT an object with controller methods
 
-router.get("/", dbController, (req, res) => {
-  return res.status(200).json(res.locals.data);
+router.get("/", apiController.getData, (req, res) => {
+  return res.status(200).json(res.locals.result);
 });
 
 module.exports = router;
